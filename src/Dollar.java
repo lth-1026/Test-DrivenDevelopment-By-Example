5820d9c311bceb78bd6e7412aec84eaa04478d81
MIYEOK <lim5570@naver.com>
2022-05-09 23:25:59 +0900

    tdd 5월 9일 공부

---
+++ src/Dollar.java
@@ -0,0 +1,15 @@
+public class Dollar {
+    private final int amount;
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+
+    public boolean equals(Object object) {
+        Dollar dollar = (Dollar) object;
+        return amount == dollar.amount;
+    }
+}
